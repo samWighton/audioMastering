960f89997f635c00f0a2c64c8feebc0e645fe967
Sam Wighton <sam@samuelwighton.com>
2014-05-16 02:44:55 +1000

    Started analysis of signal
    Mark and store all times the signal crosses from negative to a positive
    working on a method to determine the 'base frequency' of a signal given the above

--- editAudio.py
+++ editAudio.py
@@ -20,12 +20,70 @@ def main():
     for i in range(1,len(sys.argv)):
         if is_wav(sys.argv[i]):
             data = openFile(sys.argv[i])
-            
-            highPassFilter(data, 0.0001)
+            highPassFilter(data, 0.001)
+            findStartOfSine(data)
             outputFile(data)
     print ('Files processed')
     return 0
 
+def findStartOfSine(givenData,requiredAmplitude = 0.001):
+    #Find points in the file where
+    #the waveform crosses from negative to positive
+    #signifying (probably) the start of a cycle
+    #of the sine wave that is the 'base frequency'
+    #of a sample
+    points = []
+    maxAmplitude = 0
+
+    #tempTrack = []
+    #tempTrack.append(int(givenData.tracks[0]))
+    
+    for i in range(1,givenData.sampleCount):
+        #tempTrack.append(int(givenData.tracks[i]))
+        if abs(givenData.tracks[i]) > maxAmplitude:
+            maxAmplitude = abs(givenData.tracks[i])
+        if givenData.tracks[i - 1] <= 0 and givenData.tracks[i] > 0:
+            if (maxAmplitude > requiredAmplitude * pow(2,givenData.bitDepth - 1)):
+                #tempTrack[-1] = int(pow(2,givenData.bitDepth - 2))
+                maxAmplitude = 0
+                points.append(i)
+    print("Points Found = ", len(points))
+    calculateFrequency(givenData,points,50)
+    #givenData.tracks = [tempTrack[i] for i in range(givenData.sampleCount)]
+    return None
+
+def calculateFrequency(givenData,points,lowestFrequency = 50):
+    #look at distance between start of sinewaves to approximate 'base frequency'
+    gaps = []
+    for i in range(len(points)):
+        if i % 1000 == 999:
+            drawCumulative(givenData, gaps, points[i])
+            gaps = []
+        for j in range(i,i+100):#10 is arbitrary. todo, make this line better
+            if j >= len(points):
+                break
+            if j - i > givenData.sampleRate / lowestFrequency:
+                break
+            gaps.append(j-i)
+    #print('number of gaps = ', len(gaps))
+            
+def drawCumulative(givenData, gaps, startAt):
+    print('number of gaps = ', len(gaps))
+    if startAt >= givenData.sampleCount - 1000:
+        return None
+    #create tempTrack, ready to edit
+    tempTrack = []
+    for i in range(givenData.sampleCount):
+        tempTrack.append(int(givenData.tracks[i]))
+    counter = 0
+    for i in range(startAt, startAt+1000):
+        counter = 0
+        for j in range(len(gaps)):
+            if gaps[j] > i - startAt:
+                counter+=1
+        tempTrack[i] = int(pow(2,givenData.bitDepth - 2) * counter / len(gaps))#
+    givenData.tracks = [tempTrack[i] for i in range(givenData.sampleCount)]
+            
     
 def is_wav(givenString):
     #check if last 4 characters of given string are '.wav'
@@ -34,8 +92,10 @@ def is_wav(givenString):
     else:
         return False
 
-def highPassFilter(givenData, halfLife):
+def highPassFilter(givenData, halfLife = 0.001):
     #halfLife in seconds
+    #could be improved by starting at average of a quick sample of audio
+    #  rather than 0
     rollingAverage = 0
     #geoRatio will be close to 1.0
     geoRatio = pow(0.5,1/(givenData.sampleRate * halfLife))
